0338d33851f711c51e7889a64ff7f675b3c5ce7b
Cemeris <4vitalik4@gmail.com>
2021-01-08 22:05:35 +0200

    fixed x/o switch on next torn

--- script.js
+++ script.js
@@ -1,37 +1,16 @@
-console.log("Start");
 
 let cells = document.querySelectorAll(".square");
-step = 0;
+let step = 0;
 for (let cell of cells) {
-cell.addEventListener("click", function () {
-// for (let i = 1; i <= 9; i++) {
-  let symbol = document.createElement("div");
-  let i = 0;
-  symbol.textContent = i;
-  if(step % 2 === "0") {
-  symbol.textContent = "0";
-} else {
-  symbol.textContent = "x";
+    cell.addEventListener("click", function () {
+        let symbol = document.createElement("div");
+        step = step + 1;
+        if(step % 2 == 0) {
+            symbol.textContent = "0";
+        } else {
+            symbol.textContent = "x";
+        }
+        cell.append(symbol);
+    });
 }
-  cell.append(symbol);
-//}
-});
-}
-
-// let game = document.querySelector(".container");
-// for (let i = 0; i < 9; i++) {
-// game.innerHTML = '<span class="cell"></span>';
-// game.classList.add("cell");
-// game.addEventListener("click", function (event) {
-//   let step = 0;
-//   symbol.textContent = i;
-//  // if(step.classList =="cell") {
-//     if (step%2 ==0){
-//     symbol.textContent = "x";
-// } else {
-//   symbol.textContent = "0";
-// }
-//   cell.append(symbol);
-//   }
-// });
 
