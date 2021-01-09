d1cee792cfb149cce22b4608cc504ab09d737035
Julia.Zorina <julia.zorinaa@gmail.com>
2021-01-09 11:19:28 +0200

    added correct code

--- script.js
+++ script.js
@@ -1,37 +1,18 @@
 console.log("Start");
 
 let cells = document.querySelectorAll(".square");
-step = 0;
+let step = 0;
 for (let cell of cells) {
-cell.addEventListener("click", function () {
-// for (let i = 1; i <= 9; i++) {
+  cell.addEventListener("click", function () {
   let symbol = document.createElement("div");
-  let i = 0;
-  symbol.textContent = i;
-  if(step % 2 === "0") {
-  symbol.textContent = "0";
+  step = step + 1;
+  if(step % 2 == "0") {
+    symbol.textContent = "0";
 } else {
-  symbol.textContent = "x";
+    symbol.textContent = "x";
 }
   cell.append(symbol);
-//}
 });
 }
 
-// let game = document.querySelector(".container");
-// for (let i = 0; i < 9; i++) {
-// game.innerHTML = '<span class="cell"></span>';
-// game.classList.add("cell");
-// game.addEventListener("click", function (event) {
-//   let step = 0;
-//   symbol.textContent = i;
-//  // if(step.classList =="cell") {
-//     if (step%2 ==0){
-//     symbol.textContent = "x";
-// } else {
-//   symbol.textContent = "0";
-// }
-//   cell.append(symbol);
-//   }
-// });
 
